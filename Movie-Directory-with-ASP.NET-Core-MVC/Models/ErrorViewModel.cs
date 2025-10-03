465aecf678df9895e02f1ac54ff822ab2db4c6a0
Mitchell Brown <mitch7553@gmail.com>
2025-10-03 16:38:07 +0800

    Created a the Movie Directory Projecrt.

---
+++ Movie-Directory-with-ASP.NET-Core-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movie_Directory_with_ASP.NET_Core_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
